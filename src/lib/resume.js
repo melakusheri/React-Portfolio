261d9928e87fc7328036cfb1955c5c8b68cbf983
Melaku Sheri <mailangels@gmail.com>
2021-10-23 00:41:18 -0400

    add resume.js

---
+++ src/lib/resume.js
@@ -0,0 +1,35 @@
+const experience = [{
+        id: 1,
+        title: 'Account Manager',
+        company: 'RIC AIRPORT TRANSPORTATION',
+        dates: 'February 2010 to Present',
+        description: [
+            'Payroll.',
+            'Acounting.',
+
+        ]
+    },
+    {
+        id: 2,
+        title: 'Engine camshaft designer',
+        company: 'Autohaus Glinicke GmbH',
+        dates: 'August 2005 to November 2008',
+        description: [
+            'DESIGNING VW CAMSHAFTS INSIDE THE ENGINE BLOCK',
+
+        ]
+    },
+    {
+        id: 3,
+        title: 'Medical Laboratory Technician',
+        company: 'CHIPPENHAM HOSPITAL',
+        dates: 'June 2012 to August 2013',
+        description: [
+            'evaluated, and processed human specimen.',
+
+        ]
+
+    }
+];
+
+export default experience;
